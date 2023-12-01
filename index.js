f7fa2ef152d40df9453cb037042dd55c37a49aa8
Jose Aguilera <jose.maria13@hotmail.es>
2023-12-01 20:25:23 -0300

    agregar registro para compraresto

--- index.js
+++ index.js
@@ -83,6 +83,43 @@ app.get('/compras/py/agregar', async (req, res) => {
     }
 });
 
+app.get('/compras/resto/agregar', async (req, res) => {
+    try {
+        const compra = {
+            producto: "mouse",
+            cantidad: 5,
+            precio_unitario: 10000,
+            total: 50000,
+            fecha_compra: '2023-11-21'
+        };
+
+        const query = `
+                INSERT INTO compras (producto, cantidad, precio_unitario, total, fecha_compra)
+                VALUES ($1, $2, $3, $4, $5)
+                `;
+
+        await poolCompraResto.query(query,
+            [compra.producto, compra.cantidad, compra.precio_unitario, compra.total, compra.fecha_compra],
+            (error, resultado) => {
+                if (error) {
+                    console.error("Error al insertar: ", error);
+                } else {
+                    console.log("Insercion correcta: ");
+                }
+                //poolCompraresto.end();
+            }
+        );
+
+        const result = {
+            mensaje: "Registro insertado correctamente"
+        }
+
+        res.json(result);
+    } catch (error) {
+        console.error("Error grave en el servidor al insertar", error);
+        res.status(500).send("Error grave en el servidor al insertar");
+    }
+});
 
 
 
